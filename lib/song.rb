59adf459597c10f22e8a7a120e213d22ec8f64ab
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-05 01:39:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    @@genre_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each do |genre|
+      if @@genre_count.keys.include?(genre)
+        @@genre_count[genre] += 1
+      else
+        @@genre_count[genre] = 1
+      end
+    end
+    @@genre_count
+  end
+end
